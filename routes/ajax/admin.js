acc62ef06609c6611eec80348d31228eda7c50bb
TJ Horner <me@tjhorner.com>
2014-11-30 18:01:23 -0800

    Minor typo in ajax/admin

--- routes/ajax/admin.js
+++ routes/ajax/admin.js
@@ -31,7 +31,7 @@ var broadcast = function (req, res) {
         return;
     }
 
-    log.debug(req.session.user.name + '' + req.session.user.id + ' sent broadcast: ' + text);
+    log.debug(req.session.user.name + '/' + req.session.user.id + ' sent broadcast: ' + text);
 
     Chat.global.sendSystemMessage(text);
 
@@ -43,7 +43,7 @@ var broadcast = function (req, res) {
 };
 
 var restart = function (req, res) {
-    log.info(req.session.user.name + '' + req.session.user.id + ' triggered application restart');
+    log.info(req.session.user.name + '/' + req.session.user.id + ' triggered application restart');
 
     res.send(200);
 
